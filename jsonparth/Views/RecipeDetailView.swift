eafb18a4570b12f97fa248f1e7bc9fcaa7b1a403
JohanCoding1 <artur.jaja@bk.ru>
2021-11-30 10:14:07 +0300

    - add new json data - modify json parsing - modify detail view to display ingredient name

--- jsonparth/Views/RecipeDetailView.swift
+++ jsonparth/Views/RecipeDetailView.swift
@@ -22,8 +22,8 @@ struct RecipeDetailView: View {
                 Text("Ingredients")
                     .font(.headline)
                     .padding(.bottom, 8)
-                ForEach(recipe.ingredients, id: \.self) { item in
-                    Text("• " + item)
+                ForEach(recipe.ingredients) { item in
+                    Text("• " + item.name)
                         .padding(.bottom, 1)
                 }
             }
